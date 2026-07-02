fd946a96ee4479a55978b92565c26816d5a74053
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-07-02 18:53:14 -0400

    basic-01: update

--- src/basic-01.cpp
+++ src/basic-01.cpp
@@ -5,6 +5,8 @@
 using namespace peel;
 
 ////////////////////////////////////////////////////////////////////////////////
+constexpr auto inf = -1; // timeout
+
 int main(int argc, char* argv[])
 {
     Gst::init(&argc, &argv);
@@ -15,8 +17,9 @@ int main(int argc, char* argv[])
     auto bus = pipeline->get_bus();
 
     pipeline->set_state(Gst::State::PLAYING);
-    auto msg = bus->timed_pop_filtered(-1, Gst::Message::Type::ERROR_ | Gst::Message::Type::EOS);
-    if (msg->type == Gst::Message::Type::ERROR_) std::cout << "Pipeline error" << std::endl;
+    auto msg = bus->timed_pop_filtered(inf, Gst::Message::Type::ERROR_ | Gst::Message::Type::EOS);
+    if (msg->type == Gst::Message::Type::ERROR_)
+        std::cout << "An error occured! Re-run with GST_DEBUG=*:WARN" << std::endl;
 
     pipeline->set_state(Gst::State::NULL_);
     return 0;
