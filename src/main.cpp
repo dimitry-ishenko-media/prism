98846dc8abca02c999aa2b6b7a8d1baf35e7f945
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-07-05 21:22:59 -0400

    main: add asio loop

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,16 @@
 // Distributed under the GNU GPL license. See the LICENSE.md file for details.
 
 ////////////////////////////////////////////////////////////////////////////////
+#include <asio.hpp>
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello world!" << std::endl;
+
+    asio::io_context ctx;
+    auto work = asio::make_work_guard(ctx);
+
+    ctx.run();
     return 0;
 }
