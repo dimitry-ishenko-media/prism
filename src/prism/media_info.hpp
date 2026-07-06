ce832a9d3841da7f95881da2a23f93520fe9d34c
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-07-06 17:12:06 -0400

    Add audio_info bits

--- src/prism/media_info.hpp
+++ src/prism/media_info.hpp
@@ -7,14 +7,29 @@
 ////////////////////////////////////////////////////////////////////////////////
 #pragma once
 
+#include <string>
+
 ////////////////////////////////////////////////////////////////////////////////
 namespace prism
 {
+
+struct audio_info
+{
+    int rate;
+    int channels;
+    std::string format;
+};
 
 struct video_info
 {
     int width, height;
     struct { int num, den; } fps;
+};
+
+struct media_info
+{
+    audio_info audio;
+    video_info video;
 };
 
 }
