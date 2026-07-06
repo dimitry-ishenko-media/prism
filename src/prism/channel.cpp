009709ca4713325227f1b06c67192d4a87a1caf2
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-07-05 22:26:46 -0400

    channel: refactor

--- src/prism/channel.cpp
+++ src/prism/channel.cpp
@@ -6,6 +6,7 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 #include "channel.hpp"
+
 #include <format>
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -15,43 +16,43 @@ namespace prism
 channel::channel(std::string id, channel::format fmt) :
     id_{std::move(id)}, fmt_{std::move(fmt)}
 {
-    using gc = Gst::Caps;
-    using ge = Gst::ElementFactory;
-    using gp = Gst::Pipeline;
+    pipeline_ = Gst::Pipeline::create(id_.data()).ref_sink();
 
-    pipeline_ = gp::create(id_.data()).ref_sink();
+    auto create = [](auto type, auto name){
+        return Gst::ElementFactory::make(type, name).ref_sink();
+    };
 
-    auto src = ge::make("videotestsrc", "src").ref_sink();
+    auto src = create("videotestsrc", "src");
     src->set_property<int>("pattern", 2); // black
     src->set_property<bool>("is-live", true);
 
-    auto incaps = ge::make("capsfilter", "incaps").ref_sink();
-    auto caps = gc::from_string(std::format(
+    auto infil = create("capsfilter", "infil");
+    auto caps = Gst::Caps::from_string(std::format(
         "video/x-raw, width=16, height=16, framerate={}/{}",
         fmt_.fps.num, fmt_.fps.den
     ).data());
-    incaps->set_property<gc>("caps", caps);
+    infil->set_property<Gst::Caps>("caps", caps);
 
-    auto glup = ge::make("glupload", "glup").ref_sink();
-    auto glcc = ge::make("glcolorconvert", "glcc").ref_sink();
-    auto mixer= ge::make("glvideomixer", "mixer").ref_sink();
+    auto glup = create("glupload", "glup");
+    auto glcc = create("glcolorconvert", "glcc");
+    auto mixer= create("glvideomixer", "mixer");
 
-    auto outcaps = ge::make("capsfilter", "outcaps").ref_sink();
-    caps = gc::from_string(std::format(
+    auto exfil= create("capsfilter", "exfil");
+    caps = Gst::Caps::from_string(std::format(
         "video/x-raw(memory:GLMemory), format=RGBA, width={}, height={}, framerate={}/{}",
         fmt_.width, fmt_.height, fmt_.fps.num, fmt_.fps.den
     ).data());
-    outcaps->set_property<gc>("caps", caps);
+    exfil->set_property<Gst::Caps>("caps", caps);
 
-    auto tee = ge::make("tee", "tee").ref_sink();
-    auto queue = ge::make("queue", "queue").ref_sink();
+    auto tee = create("tee", "tee");
+    auto queue = create("queue", "queue");
 
-    auto sink = ge::make("fakesink", "sink").ref_sink();
+    auto sink = create("fakesink", "sink");
     sink->set_property<bool>("sync", true);
 
     ////////////////////
-    pipeline_->add_many(src, incaps, glup, glcc, mixer, outcaps, tee, queue, sink);
-    src->link_many(incaps, glup, glcc);
+    pipeline_->add_many(src, infil, glup, glcc, mixer, exfil, tee, queue, sink);
+    src->link_many(infil, glup, glcc);
 
     auto inpad = mixer->get_request_pad("sink_%u");
     inpad->set_property<int>("width", fmt_.width);
@@ -61,7 +62,7 @@ channel::channel(std::string id, channel::format fmt) :
     auto outpad = glcc->get_static_pad("src");
     outpad->link(inpad);
 
-    mixer->link_many(outcaps, tee, queue, sink);
+    mixer->link_many(exfil, tee, queue, sink);
 }
 
 channel::~channel()
